a16a0f590aa2069296da801aab67001c31097426
binhan <qweqazhank123@gmail.com>
2023-06-02 01:24:13 +0800

    next_block_type

--- final-project/Form1.Designer.cs
+++ final-project/Form1.Designer.cs
@@ -38,7 +38,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
