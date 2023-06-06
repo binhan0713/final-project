b69c7ac7b26d84e259dfa36deacd9a2ce6c3c2fb
陳駿杰 <zxas040866@gmail.com>
2023-06-06 20:13:47 +0800

    1234756

--- final-project/Form1.cs
+++ final-project/Form1.cs
@@ -165,19 +165,19 @@ namespace final_project
                     end = true;
                     timer1.Enabled = false;
                     // 讀取 JSON 檔案中的分數紀錄，並將它們反序列化為 List<int>
-                    string json = File.ReadAllText("./score.json");
-                    List<int> scores = JsonSerializer.Deserialize<List<int>>(json);
+                    //string json = File.ReadAllText("./score.json");
+                    //List<int> scores = JsonSerializer.Deserialize<List<int>>(json);
 
                     // 在遊戲結束時，將玩家的分數加入 scores 列表中
-                    int newScore =Convert.ToInt32(score);
-                    scores.Add(newScore);
+                    //int newScore =Convert.ToInt32(score);
+                    //scores.Add(newScore);
 
                     // 根據分數排序 scores 列表，以便前十名分數最高的玩家排在前面
-                    scores = scores.OrderByDescending(s => s).Take(10).ToList();
+                    //scores = scores.OrderByDescending(s => s).Take(10).ToList();
 
                     // 將 scores 列表序列化為 JSON 字串，並將其寫入 scores.json 檔案中
-                    json = JsonSerializer.Serialize(scores);
-                    File.WriteAllText("scores.json", json);
+                    //json = JsonSerializer.Serialize(scores);
+                    //File.WriteAllText("scores.json", json);
                     MessageBox.Show("Game Over! 請按下Enter重新開始");
                     return;
                 };
